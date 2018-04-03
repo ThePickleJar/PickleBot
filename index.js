a4138b6290bc3e107ccd9532016bb00ccf193b57
ThePickleJar <38021787+ThePickleJar@users.noreply.github.com>
2018-04-02 22:19:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,9 @@
 
 }
 
-var express = require('express');
-
-var app = express();
-
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 
 const commando = require('discord.js-commando');
